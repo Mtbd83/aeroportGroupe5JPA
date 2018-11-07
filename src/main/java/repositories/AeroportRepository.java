dac4ef78ea06411bb562f7a16fb3aa86391defc3
Unknown <saidani.tanguy@gmail.com>
2018-11-07 17:30:37 +0100

    maj classes services ville, aeroport et compagnie

--- src/main/java/repositories/AeroportRepository.java
+++ src/main/java/repositories/AeroportRepository.java
@@ -6,4 +6,6 @@ import model.Aeroport;
 
 public interface AeroportRepository extends JpaRepository<Aeroport, Integer> {
 
+	Aeroport findByNom(String nom);
+
 }
--- src/main/java/repositories/VilleRepository.java
+++ src/main/java/repositories/VilleRepository.java
@@ -6,4 +6,5 @@ import model.Ville;
 
 public interface VilleRepository extends JpaRepository<Ville, Integer> {
 
+	Ville findByNom(String nom);
 }
