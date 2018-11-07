afb0ac6a071f783c1b223aa5ae180810973e3942
Mtbd83 <m.tribaudeau@gmail.com>
2018-11-07 17:27:50 +0100

    Service Reservation
    Service

--- src/main/java/repositories/VolRepository.java
+++ src/main/java/repositories/VolRepository.java
@@ -1,8 +1,17 @@
 package repositories;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import model.Vol;
 
 public interface VolRepository extends JpaRepository<Vol, Integer>  {
+	
+	List<Vol> findByDateDepart(Date dateDepart);
+	List<Vol> findByDateArrivee(Date dateArrivee);
+	
+	List<Vol> findByHeureDepartBetween (Date heureDepart1, Date heureDepart2 );
+	List<Vol> findByHeureArriveeBetween (Date heureArivee1, Date heureArrivee2 );
 	
 }
---
+++ src/main/java/service/VolService.java
@@ -0,0 +1,36 @@
+package service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import model.Vol;
+
+@Service
+public class VolService {
+	
+	@Autowired
+	private VolService volService;
+	
+	
+	
+	public void createVol() {
+		
+	}
+
+	
+	
+	public void deleteVol(Integer idVol){
+		
+	}
+	
+	
+	public List<Vol> showAll(){
+		return null;
+	}
+	
+
+}
+
